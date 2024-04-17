d8a9d8ec7c648935070d52b7276a095bebb1f01e
Jay Unruh <jru@stowers.org>
2024-04-17 14:25:13 -0500

    update

---
+++ drafts/myimport.py
@@ -0,0 +1,9 @@
+def findnthsubstring(mystr,substr,n=2):
+    #it's a good idea to describe what your function does with comments
+    #this function finds the nth instance of substr in mystr
+    pos=0
+    i=0
+    while(i<n):
+        pos=mystr.find(substr,pos+1)
+        i+=1
+    return pos
---
+++ update_github.sh
@@ -0,0 +1,6 @@
+cp * /Users/jru/Documents/GitHub/python_introDS_course/
+cd /Users/jru/Documents/GitHub/python_introDS_course
+git add *
+git commit -m "update"
+git push --all --repo=https://github.com/jayunruh/python_introDS_course
+cd "/Users/jru/Documents/IPython_Notebooks/python_introDS_course/"
