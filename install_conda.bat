03ee0c8be0b41c95f77f7a000911ef06408bc3ad
Jay Unruh <jru@stowers.org>
2025-07-15 16:00:01 -0500

    win installer edit

--- install_conda.bat
+++ install_conda.bat
@@ -2,12 +2,12 @@ REM first get on the c drive
 c:
 cd %USERPROFILE%\Documents
 REM download the installer
-curl -O https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-Windows-x86_64.exe
+curl -L -O https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-Windows-x86_64.exe
 REM install it quietly
 start /wait "" Miniforge3-Windows-x86_64.exe /InstallationType=JustMe /RegisterPython=0 /S /D=%UserProfile%\Miniforge3
 
 REM setup the .condarc file
 echo channels: > %USERPROFILE%\.condarc
-echo    --conda-forge >> %USERPROFILE%\.condarc
-echo    --bioconda >> %USERPROFILE%\.condarc
+echo    - conda-forge >> %USERPROFILE%\.condarc
+echo    - bioconda >> %USERPROFILE%\.condarc
 echo auto_activate_base: false >> %USERPROFILE%\.condarc
